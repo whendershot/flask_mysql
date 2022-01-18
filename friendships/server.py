082157e5632300534ea539e8c6e6f21d3654fdb4
William Hendershot <whendershot1@gmail.com>
2022-01-18 12:02:50 +0900

    Challenge setup

---
+++ friendships/server.py
@@ -0,0 +1,16 @@
+from flask import redirect
+from flask_app import app
+from flask_app.controllers import dojos, ninjas
+
+@app.route('/')
+def index():
+    return redirect('/dojos')
+
+@app.route('/', defaults={'u_path' : ''})
+@app.route('/<path:u_path>')
+def catch_all(u_path):
+    print(repr(u_path))
+    return f'Sorry! No response. Try again. But how did you fall into here?<br>{u_path}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
