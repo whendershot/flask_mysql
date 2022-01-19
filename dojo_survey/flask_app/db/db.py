b7d8af2ef255e2fe5f7a73b403e816763f4c5db6
William Hendershot <whendershot1@gmail.com>
2022-01-19 09:50:31 +0900

    Challenge setup

---
+++ dojo_survey/flask_app/db/db.py
@@ -0,0 +1,50 @@
+dojo_locations = [
+    {
+        "id" : 1,
+        "name" : "San Jose",
+        "description" : "Something about San Jose"
+    },
+    {
+        "id" : 2,
+        "name" : "Chicago",
+        "description" : "Something about Chicago"
+    },
+    {
+        "id" : 3,
+        "name" : "Seattle",
+        "description" : "Something about Washington"
+    },
+    {
+        "id" : 4,
+        "name" : "Hong Kong",
+        "description" : "Something about Hong Kong"
+    },
+    {
+        "id" : 5,
+        "name" : "Bellevue",
+        "description" : "Something about the other Washington"
+    }
+]
+
+available_programming_languanges = [
+    {
+        "id" : 1,
+        "name" : "Python"
+    },
+    {
+        "id" : 2,
+        "name" : "JavaScript"
+    },
+    {
+        "id" : 3,
+        "name" : "HTML"
+    },
+    {
+        "id" : 4,
+        "name" :"Ruby"
+    },
+    {
+        "id" : 5,
+        "name" : "C#"
+    }
+]
